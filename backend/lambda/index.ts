b5414565a5c28f27f9213485925cf76a372595ad
Melanie <melanie.mazanec@gmail.com>
2025-04-30 17:02:12 -0400

    Hello airtable

--- backend/lambda/index.ts
+++ backend/lambda/index.ts
@@ -1,14 +1,55 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/aws-lambda'
 import { serve } from '@hono/node-server'
+import * as dotenv from "dotenv";
+import Airtable = require('airtable');
 
 const app = new Hono()
+const env = dotenv.config({ path: "./.env" }).parsed;
+const AIRTABLE_API_KEY = env?.AIRTABLE_API_KEY || '';
+const AIRTABLE_BASE_ID = env?.AIRTABLE_BASE_ID || '';
+var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID);
 
 app.get('/', (c) => c.text('Hello Hono!'))
 
-app.get('/updates', (c) => c.text('Get updates!'))
+// TODO filter by date
+app.get('/updates', async (c) => {
+    base('Updates').select({
+        // Selecting the first 3 records in Grid view:
+        maxRecords: 30,
+        view: "Grid view"
+    }).eachPage(function page(records: any, fetchNextPage: Function) {
+        // This function (`page`) will get called for each page of records.
 
-app.post('/updates', (c) => c.text('Post updates!'))
+        records.forEach(function (record: any) {
+            console.log(record);
+        });
+
+        fetchNextPage();
+
+    }, function done(err: any) {
+        if (err) { console.error(err); return; }
+    });
+    return c.text('Get updates!')
+})
+
+// TODO update blockers etc
+// app.post('/updates', (c) => {
+//     base('Updates').create([
+//         {
+//           "fields": {}
+//         }
+//       ], function(err, records) {
+//         if (err) {
+//           console.error(err);
+//           return;
+//         }
+//         records.forEach(function (record) {
+//           console.log(record.getId());
+//         });
+//       });
+//     return c.text('Post updates!')
+// })
 
 app.get('/projects/:id', (c) => c.text('Get project!'))
 
