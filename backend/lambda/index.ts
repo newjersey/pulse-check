ce40562146dabe0848601626e1a657a75cd9fb02
Melanie <melanie.mazanec@gmail.com>
2025-04-30 15:59:45 -0400

    Add backend dev server, some deployment boilerplate

--- backend/lambda/index.ts
+++ backend/lambda/index.ts
@@ -1,8 +1,17 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/aws-lambda'
+import { serve } from '@hono/node-server'
 
 const app = new Hono()
 
 app.get('/', (c) => c.text('Hello Hono!'))
+
+app.get('/updates', (c) => c.text('Get updates!'))
+
+app.post('/updates', (c) => c.text('Post updates!'))
+
+app.get('/projects/:id', (c) => c.text('Get project!'))
+
+serve({ fetch: app.fetch, port: 3001 })
 
 export const handler = handle(app)
--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,23 +1,61 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda'
-import * as apigw from 'aws-cdk-lib/aws-apigateway'
+import { Runtime, FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda'
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Distribution, ViewerProtocolPolicy, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { join } from 'path';
 
-export class BackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class BackendStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const fn = new NodejsFunction(this, 'lambda', {
       entry: 'lambda/index.ts',
       handler: 'handler',
-      runtime: lambda.Runtime.NODEJS_20_X,
+      runtime: Runtime.NODEJS_20_X,
     })
     fn.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE,
+      authType: FunctionUrlAuthType.NONE,
     })
-    new apigw.LambdaRestApi(this, 'myapi', {
+    new LambdaRestApi(this, 'myapi', {
       handler: fn,
     })
+
+
+    // Create an S3 bucket to host the React app
+    const websiteBucket = new Bucket(this, 'WebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    });
+
+    // Deploy React app to S3
+    new BucketDeployment(this, 'DeployWebsite', {
+      sources: [Source.asset(join(__dirname, '../react-app/build'))],
+      destinationBucket: websiteBucket,
+    });
+
+    // Create a CloudFront Origin Access Identity
+    const originAccessIdentity = new OriginAccessIdentity(this, 'OAI');
+
+    // Grant CloudFront access to the S3 bucket
+    websiteBucket.grantRead(originAccessIdentity);
+
+    // Create a CloudFront distribution to serve the React app
+    const distribution = new Distribution(this, 'WebsiteDistribution', {
+      defaultBehavior: {
+        origin: new S3BucketOrigin(websiteBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+    });
+
+    // Output the CloudFront URL
+    new CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.distributionDomainName,
+    });
   }
 }
