c1aa8375e528e90b60d7b501d956ea55f0f6eb2f
Melanie <melanie.mazanec@gmail.com>
2025-05-01 10:23:19 -0400

    Get airtable data from API

--- backend/lambda/index.ts
+++ backend/lambda/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/aws-lambda'
 import { serve } from '@hono/node-server'
+import { cors } from 'hono/cors'
 import * as dotenv from "dotenv";
 import Airtable = require('airtable');
 
@@ -8,29 +9,42 @@ const app = new Hono()
 const env = dotenv.config({ path: "./.env" }).parsed;
 const AIRTABLE_API_KEY = env?.AIRTABLE_API_KEY || '';
 const AIRTABLE_BASE_ID = env?.AIRTABLE_BASE_ID || '';
-var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID);
+const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID);
+
+const tableNames = {
+    UPDATES: 'Updates',
+    PROJECTS: 'Projects',
+    BLOCKERS: 'Blockers',
+}
+
+// TODO make time a param
+async function getUpdates() {
+    try {
+        const response = await base(tableNames.UPDATES).select({
+            view: "Grid view",
+            // filterByFormula: `IS_AFTER({ Created }, "1/1/2025")`
+        })
+        const records: any = []
+        await response.eachPage(function page(_records: any, fetchNextPage: Function) {
+            _records.forEach(function (record: any) {
+                records.push(record.fields)
+            });
+            fetchNextPage();
+        });
+        return records
+    } catch(e) {
+        console.log(e)
+    }
+}
 
 app.get('/', (c) => c.text('Hello Hono!'))
 
 // TODO filter by date
+app.use('*', cors({ origin: ['http://localhost:5173'] }))
+
 app.get('/updates', async (c) => {
-    base('Updates').select({
-        // Selecting the first 3 records in Grid view:
-        maxRecords: 30,
-        view: "Grid view"
-    }).eachPage(function page(records: any, fetchNextPage: Function) {
-        // This function (`page`) will get called for each page of records.
-
-        records.forEach(function (record: any) {
-            console.log(record);
-        });
-
-        fetchNextPage();
-
-    }, function done(err: any) {
-        if (err) { console.error(err); return; }
-    });
-    return c.text('Get updates!')
+    const updates = await getUpdates();
+    return c.json(updates);
 })
 
 // TODO update blockers etc
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [updates, setUpdates] = useState<Object[]>()
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/updates'); // Replace with your backend URL
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setUpdates(result);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        // setLoading(false);
+      }
+    };
+    // Call the async function
+    fetchData();
+  }, [setUpdates])
+
+  console.log(updates)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Resx dashboard</h1>
+      <ul>
+        {updates && updates.map(u => (<li>{`Update ${u["ID"]} does ${ u["Needs intervention"] == 0 ? 'not ': ''}need attention`}</li>))}
+      </ul>
     </>
   )
 }
