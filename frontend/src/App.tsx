9462928ede1fe24344483e99a8256e1408357c0d
Melanie <melanie.mazanec@gmail.com>
2025-05-09 19:20:04 -0400

    Add timeline view toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router';
+import { Routes, Route, Link, useLocation } from 'react-router';
 import Projects from './pages/Projects';
 import './App.css'
 import SubmitUpdate from './pages/SubmitUpdate';
@@ -7,6 +7,7 @@ import Login from './pages/Login';
 import Project from './pages/Project';
 
 function App() {
+  const { pathname, search } = useLocation()
   return (
     <DataContextProvider>
       <header className="usa-header usa-header--basic">
@@ -17,16 +18,22 @@ function App() {
               <Link to="/" className="usa-logo__text">Resx dashboard</Link>
             </div>
           </div>
+          {(pathname === '/' || pathname === '/projects') && <nav className='usa-nav'>
+            {search === '?view=timeline' ?
+              <Link to={{ pathname: "/projects", search: "" }}>Status view</Link> :
+              <Link to={{ pathname: "/projects", search: "?view=timeline" }}>Timeline view</Link>
+            }
+          </nav>}
         </div>
       </header>
       <main id="main-content" className="grid-container">
         <Routes>
-          <Route path="/login" element={<Login/>} />
-          <Route path="/" element={<Projects/>} />
-          <Route path="/projects" element={<Projects/>} />
-          <Route path="/projects/:projectId" element={<Project/>} />
-          <Route path="/projects/:projectId/update" element={<SubmitUpdate  />} />
-      </Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Projects />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:projectId" element={<Project />} />
+          <Route path="/projects/:projectId/update" element={<SubmitUpdate />} />
+        </Routes>
       </main>
     </DataContextProvider>
   )
--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -2,11 +2,15 @@ import PageTemplate from '../components/PageTemplate';
 import { Project, useDataContext } from '../utils/DataContext';
 import { useEffect, useState } from 'react';
 import ProjectRow from '../components/ProjectRow';
+import { useLocation } from 'react-router';
+import ProjectMilestoneTimeline from '../components/ProjectMilestoneTimeline';
 
 export default function () {
   const { projects } = useDataContext();
   const [activeProjects, setActiveProjects] = useState<Project[]>([])
   // const [sunsetProjects, setSunsetProjects] = useState<Project[]>([])
+
+  const { search } = useLocation();
 
   useEffect(() => {
     const active = projects.filter(p => p.Phase !== 'Sunset')
@@ -16,7 +20,9 @@ export default function () {
   }, [projects])
 
   return <PageTemplate title="Projects">
-    {activeProjects && activeProjects.map(project => <ProjectRow project={project} />)}
+    {activeProjects && activeProjects.map(project =>
+      search === '?view=timeline' ? <ProjectMilestoneTimeline key={project.id} project={project} /> : <ProjectRow key={project.id} project={project} />
+    )}
     {/* <h2>Sunset and handed-off projects</h2>
     {sunsetProjects && sunsetProjects.map(project => <ProjectRow project={project} />)} */}
   </PageTemplate>
