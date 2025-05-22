34ef0afd3260d25bcc9e3b7cd8c64dde67c97936
Melanie <melanie.mazanec@gmail.com>
2025-05-22 16:52:09 -0400

    Show/hide/ditch form on project change

--- frontend/src/components/FormFields/ProjectField.tsx
+++ frontend/src/components/FormFields/ProjectField.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
+import { ChangeEvent, useEffect } from "react";
 import { useFormContext } from "../../contexts/FormContext"
 import { useDataContext } from "../../contexts/DataContext";
 import useProject from "../../utils/useProject";
+import { useLocation, useNavigate } from "react-router";
 
 export default function () {
   const { projects } = useDataContext()
   const { handleInputChange, addFields } = useFormContext();
   const { projectId } = useProject();
+  const navigate = useNavigate()
+  const location = useLocation()
 
   const field = {
     name: 'project',
@@ -20,10 +23,15 @@ export default function () {
     addFields([field])
   }, [])
 
+  function handleProjectChange(e: ChangeEvent<HTMLSelectElement>) {
+    handleInputChange(e)
+    navigate({...location, search: `?projectId=${e.target.value}`}, { replace: true })
+  }
+
   return <>
     <label className="usa-label" htmlFor={field.id}>{field.label}</label>
-    <select className="usa-select" name={field.name} id={field.id} required={field.required} onChange={handleInputChange} defaultValue={field.value}>
-      <option value={undefined}>- Choose a project -</option>
+    <select className="usa-select" name={field.name} id={field.id} required={field.required} onChange={handleProjectChange} defaultValue={field.value}>
+      <option value="">- Choose a project -</option>
       {projects && Object.keys(projects).map(key => (
         <option value={key} key={key}>{projects[key]?.Name}</option>
       ))}
--- frontend/src/pages/AddUpdate/UpdateForm.tsx
+++ frontend/src/pages/AddUpdate/UpdateForm.tsx
@@ -5,24 +5,28 @@ import ProjectField from "../../components/FormFields/ProjectField";
 import UpdateStatus from "../../components/FormFields/UpdateStatus";
 import MetricUpdates from "../../components/FormFields/MetricUpdates";
 import ProjectNeeds from "../../components/FormFields/ProjectNeeds";
+import useProject from "../../utils/useProject";
+import React from "react";
 
 
 export default function () {
   const { postData } = useDataContext();
   const { fields } = useFormContext();
+  const { projectId } = useProject();
 
   function onSubmit(e: any) {
     e.preventDefault();
-    console.log(fields)
     postData('update', fields)
   }
 
-  return <form>
+  return <form onSubmit={() => { }}>
     <ProjectField />
-    <UpdateDescription />
-    <UpdateStatus />
-    <MetricUpdates />
-    <ProjectNeeds />
+    {projectId && <React.Fragment key={projectId}>
+      <UpdateDescription />
+      <UpdateStatus />
+      <MetricUpdates />
+      <ProjectNeeds />
+    </React.Fragment>}
     <input type="submit" className="usa-button margin-y-4" value="Submit" onClick={onSubmit} />
   </form>
 }
